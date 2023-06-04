5c8621695f1a9152fed8ef9603f1f3f4ce563340
Swayzer <106442170+Swayz3@users.noreply.github.com>
2023-06-04 10:56:28 -0400

    Update d2looter.py

--- d2looter.py
+++ d2looter.py
@@ -8,33 +8,36 @@ screen_width = root.winfo_screenwidth()
 screen_height = root.winfo_screenheight()
 winx = (round(screen_width/3))
 winy = (round(screen_height/3))
+# Define a StringVar variable
+text_var = tk.StringVar()
 # Create the overlay window as a translucent Toplevel window
 overlay1 = tk.Toplevel(root)
 overlay1.overrideredirect(True)
 overlay1.attributes('-alpha', .5)  # Set the overlay transparency (0.5 for 50% transparency)
 overlay1.geometry(f'{screen_width}x{winy}+0+0')
 overlay1.configure(bg='black')
-
+label2 = tk.Label(overlay1, text="Scanning", font=("Arial", 20), fg="white", bg="black")
+label2.pack(pady=50)
 # Create the left rectangle
 overlay2 = tk.Toplevel(root)
 overlay2.overrideredirect(True)
 overlay2.attributes('-alpha', .5)  # Set the overlay transparency (0.5 for 50% transparency)
 overlay2.geometry(f'{winx}x{winy}+0+{winy}')
 overlay2.configure(bg='black')
-
 # Create the right rectangle
 overlay3 = tk.Toplevel(root)
 overlay3.overrideredirect(True)
 overlay3.attributes('-alpha', .5)  # Set the overlay transparency (0.5 for 50% transparency)
 overlay3.geometry(f'{winx}x{winy}+{2*winx}+{winy}')
 overlay3.configure(bg='black')
-
 # Create the bottom rectangle
 overlay4 = tk.Toplevel(root)
 overlay4.overrideredirect(True)
 overlay4.attributes('-alpha', .5)  # Set the overlay transparency (0.5 for 50% transparency)
 overlay4.geometry(f'{screen_width}x{winy}+0+{2*winy}')
 overlay4.configure(bg='black')
+label = tk.Label(overlay4, textvariable=text_var, font=("Arial", 20), fg="white", bg="black")
+label.pack(pady=50)
 root.update()
 def show():
     root.update()
@@ -53,12 +56,16 @@ def loot():
     Coord = pyautogui.locateCenterOnScreen('unique.png', region=(winx, winy, winx, winy))
     Coord2 = pyautogui.locateCenterOnScreen('rune.png', region=(winx, winy, winx, winy))
     if Coord is None and Coord2 is None:
-        print("No loot")
+        text_var.set("No loot")
+        root.update()
     elif Coord is not None:
         pyautogui.click(Coord.x, Coord.y)
+        text_var.set("Unique Loot Found")
+        root.update()
     elif Coord2 is not None:
         pyautogui.click(Coord2.x, Coord2.y)
-    
+        text_var.set("Rune Found")
+        root.update()
 
 def start():
     global auto_loot
